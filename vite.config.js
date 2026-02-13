47922e83a6310c1d653555ad6258e2c9bfea491e
rileyhales <rileyhales1@gmail.com>
2026-02-12 17:24:40 -0700

    render app date version to html

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,19 @@
-export default {
+import {defineConfig} from "vite"
+
+const buildDate = new Date()
+const appVersionDate = [
+  buildDate.getUTCFullYear(),
+  String(buildDate.getUTCMonth() + 1).padStart(2, "0"),
+  String(buildDate.getUTCDate()).padStart(2, "0"),
+].join("")
+
+const injectAppVersionDate = () => ({
+  name: "inject-app-version-date",
+  transformIndexHtml: html => html.replaceAll("%APP_VERSION_DATE%", appVersionDate),
+})
+
+export default defineConfig({
+  plugins: [injectAppVersionDate()],
   worker: {
     format: 'es' // Use ES modules in workers
   },
@@ -6,6 +21,6 @@ export default {
     target: 'esnext'
   },
   define: {
-    global: 'globalThis'
+    global: 'globalThis',
   }
-}
+})
